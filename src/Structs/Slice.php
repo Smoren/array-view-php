65271f821dcab2cb4c13e6565b73901f6e6ae433
Smoren <ofigate@gmail.com>
2024-03-12 15:15:08 +0300

    New tests.

--- src/Structs/Slice.php
+++ src/Structs/Slice.php
@@ -38,7 +38,8 @@ class Slice
         }
 
         if (!self::isSliceString($s)) {
-            throw new ValueError("Invalid slice: \"{$s}\".");
+            $str = \is_scalar($s) ? "{$s}" : gettype($s);
+            throw new ValueError("Invalid slice: \"{$str}\".");
         }
 
         $slice = self::parseSliceString($s);
@@ -81,6 +82,33 @@ class Slice
     }
 
     /**
+     * @param mixed $s
+     *
+     * @return bool
+     */
+    public static function isSliceArray($s): bool
+    {
+        if (!\is_array($s)) {
+            return false;
+        }
+
+        if (!(\count($s) >= 0 && \count($s) <= 3)) {
+            return false;
+        }
+
+        foreach ($s as $key => $item) {
+            if (\is_string($key)) {
+                return false;
+            }
+            if ($item !== null && (!\is_numeric($item) || \is_float($item + 0))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
      * @param int|null $start
      * @param int|null $end
      * @param int|null $step
@@ -93,11 +121,11 @@ class Slice
     }
 
     /**
-     * @param int $containerLength
+     * @param int $containerSize
      *
      * @return NormalizedSlice
      */
-    public function normalize(int $containerLength): NormalizedSlice
+    public function normalize(int $containerSize): NormalizedSlice
     {
         // TODO: Need refactor
         $step = $this->step ?? 1;
@@ -108,25 +136,25 @@ class Slice
 
         $defaultEnd = ($step < 0 && $this->end === null) ? -1 : null;
 
-        $start = $this->start ?? ($step > 0 ? 0 : $containerLength - 1);
-        $end = $this->end ?? ($step > 0 ? $containerLength : -1);
+        $start = $this->start ?? ($step > 0 ? 0 : $containerSize - 1);
+        $end = $this->end ?? ($step > 0 ? $containerSize : -1);
 
         $start = intval(round($start));
         $end = intval(round($end));
         $step = intval(round($step));
 
-        $start = Util::normalizeIndex($start, $containerLength, false);
-        $end = Util::normalizeIndex($end, $containerLength, false);
+        $start = Util::normalizeIndex($start, $containerSize, false);
+        $end = Util::normalizeIndex($end, $containerSize, false);
 
-        if ($step > 0 && $start >= $containerLength) {
-            $start = $end = $containerLength - 1;
+        if ($step > 0 && $start >= $containerSize) {
+            $start = $end = $containerSize - 1;
         } elseif ($step < 0 && $start < 0) {
             $start = $end = 0;
             $defaultEnd = 0;
         }
 
-        $start = $this->squeezeInBounds($start, 0, $containerLength - 1);
-        $end = $this->squeezeInBounds($end, $step > 0 ? 0 : -1, $containerLength);
+        $start = $this->squeezeInBounds($start, 0, $containerSize - 1);
+        $end = $this->squeezeInBounds($end, $step > 0 ? 0 : -1, $containerSize);
 
         if (($step > 0 && $end < $start) || ($step < 0 && $end > $start)) {
             $end = $start;
@@ -150,6 +178,9 @@ class Slice
      */
     private static function parseSliceString(string $s): array
     {
+        if ($s === '') {
+            return [];
+        }
         return array_map(fn($x) => trim($x) === '' ? null : \intval(trim($x)), \explode(':', $s));
     }
 
---
+++ tests/unit/Structs/SliceTest.php
@@ -0,0 +1,275 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smoren\ArrayView\Tests\Unit\Structs;
+
+use Smoren\ArrayView\Exceptions\ValueError;
+use Smoren\ArrayView\Structs\NormalizedSlice;
+use Smoren\ArrayView\Structs\Slice;
+
+class SliceTest extends \Codeception\Test\Unit
+{
+    /**
+     * @dataProvider dataProviderForTrue
+     */
+    public function testIsSliceTrue(string $input)
+    {
+        $this->assertTrue(Slice::isSlice($input));
+        $this->assertTrue(Slice::isSliceString($input));
+    }
+
+    /**
+     * @dataProvider dataProviderForFalse
+     */
+    public function testIsSliceFalse($input)
+    {
+        $this->assertFalse(Slice::isSlice($input));
+        $this->assertFalse(Slice::isSliceString($input));
+    }
+
+    /**
+     * @dataProvider dataProviderForFalse
+     */
+    public function testSliceError($input)
+    {
+        $this->expectException(ValueError::class);
+        $strInput = \is_scalar($input) ? "{$input}" : gettype($input);
+        $this->expectExceptionMessage("Invalid slice: \"{$strInput}\"");
+
+        Slice::toSlice($input);
+    }
+
+    /**
+     * @dataProvider dataProviderForToSlice
+     */
+    public function testToSlice($input, array $expected)
+    {
+        $actual = Slice::toSlice($input);
+        $expectedSlice = new Slice(...$expected);
+
+        $this->assertSame($expectedSlice->start, $actual->start);
+        $this->assertSame($expectedSlice->end, $actual->end);
+        $this->assertSame($expectedSlice->step, $actual->step);
+    }
+
+    /**
+     * @dataProvider dataProviderForSliceToString
+     */
+    public function testSliceToString(string $input, string $expected)
+    {
+        $slice = Slice::toSlice($input);
+        $this->assertSame($expected, $slice->toString());
+    }
+
+    /**
+     * @dataProvider dataProviderForSliceNormalize
+     */
+    public function testSliceNormalize(string $input, int $size, string $expected, array $expectedIndexes)
+    {
+        $slice = Slice::toSlice($input);
+        $normalizedSlice = $slice->normalize($size);
+
+        $this->assertInstanceOf(NormalizedSlice::class, $normalizedSlice);
+        $this->assertSame($expected, $normalizedSlice->toString());
+        $this->assertSame($expectedIndexes, [...$normalizedSlice]);
+    }
+
+    /**
+     * @dataProvider dataProviderForIsSliceArrayTrue
+     */
+    public function testIsSliceArrayTrue(array $input)
+    {
+        $this->assertTrue(Slice::isSliceArray($input));
+    }
+
+    /**
+     * @dataProvider dataProviderForIsSliceArrayFalse
+     */
+    public function testIsSliceArrayFalse($input)
+    {
+        $this->assertFalse(Slice::isSliceArray($input));
+    }
+
+    public function dataProviderForTrue(): array
+    {
+        return [
+            [':'],
+            ['::'],
+            ['0:'],
+            ['1:'],
+            ['-1:'],
+            ['0::'],
+            ['1::'],
+            ['-1::'],
+            [':0'],
+            [':1'],
+            [':-1'],
+            [':0:'],
+            [':1:'],
+            [':-1:'],
+            ['0:0:'],
+            ['1:1:'],
+            ['-1:-1:'],
+            ['1:1:-1'],
+            ['-1:-1:-1'],
+            ['1:2:3'],
+        ];
+    }
+
+    public function dataProviderForFalse(): array
+    {
+        return [
+            [''],
+            ['0'],
+            ['1'],
+            ['1:::'],
+            [':1::'],
+            ['::1:'],
+            [':::1'],
+            ['test'],
+            ['[::]'],
+            ['a:b:c'],
+            [0],
+            [1],
+            [1.1],
+            [true],
+            [false],
+            [null],
+            [[]],
+            [[1, 2, 3]],
+            [[null]],
+            [new \ArrayObject([])],
+            [['a' => 1]],
+        ];
+    }
+
+    public function dataProviderForToSlice(): array
+    {
+        return [
+            [':', [null, null, null]],
+            ['::', [null, null, null]],
+            ['0:', [0, null, null]],
+            ['1:', [1, null, null]],
+            ['-1:', [-1, null, null]],
+            ['0::', [0, null, null]],
+            ['1::', [1, null, null]],
+            ['-1::', [-1, null, null]],
+            [':0', [null, 0, null]],
+            [':1', [null, 1, null]],
+            [':-1', [null, -1, null]],
+            [':0:', [null, 0, null]],
+            [':1:', [null, 1, null]],
+            [':-1:', [null, -1, null]],
+            ['0:0:', [0, 0, null]],
+            ['1:1:', [1, 1, null]],
+            ['-1:-1:', [-1, -1, null]],
+            ['1:1:-1', [1, 1, -1]],
+            ['-1:-1:-1', [-1, -1, -1]],
+            ['1:2:3', [1, 2, 3]],
+        ];
+    }
+
+    public function dataProviderForSliceToString(): array
+    {
+        return [
+            [':', '::'],
+            ['::', '::'],
+            ['0:', '0::'],
+            ['1:', '1::'],
+            ['-1:', '-1::'],
+            ['0::', '0::'],
+            ['1::', '1::'],
+            ['-1::', '-1::'],
+            [':0', ':0:'],
+            [':1', ':1:'],
+            [':-1', ':-1:'],
+            [':0:', ':0:'],
+            [':1:', ':1:'],
+            [':-1:', ':-1:'],
+            ['0:0:', '0:0:'],
+            ['1:1:', '1:1:'],
+            ['-1:-1:', '-1:-1:'],
+            ['1:1:-1', '1:1:-1'],
+            ['-1:-1:-1', '-1:-1:-1'],
+            ['1:2:3', '1:2:3'],
+        ];
+    }
+
+    public function dataProviderForSliceNormalize(): array
+    {
+        return [
+            [':', 0, '0:0:1', []],
+            ['::', 1, '0:1:1', [0]],
+            ['0:', 2, '0:2:1', [0, 1]],
+            ['1:', 5, '1:5:1', [1, 2, 3, 4]],
+            ['-1:', 3, '2:3:1', [2]],
+            ['0::', 10, '0:10:1', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]],
+            ['1::', 0, '0:0:1', []],
+            ['-1::', 0, '0:0:1', []],
+            [':0', 1, '0:0:1', []],
+            [':1', 2, '0:1:1', [0]],
+            [':-1', 5, '0:4:1', [0, 1, 2, 3]],
+            [':0:', 3, '0:0:1', []],
+            [':1:', 1, '0:1:1', [0]],
+            [':-1:', 3, '0:2:1', [0, 1]],
+            ['0:0:', 3, '0:0:1', []],
+            ['1:1:', 3, '1:1:1', []],
+            ['-1:-1:', 10, '9:9:1', []],
+            ['1:1:-1', 10, '1:1:-1', []],
+            ['-1:-1:-1', 10, '9:9:-1', []],
+            ['1:2:3', 10, '1:2:3', [1]],
+            ['1:2:3', 1, '0:0:3', []],
+            ['::-1', 1, '0:-1:-1', [0]],
+            ['1::-1', 1, '0:-1:-1', [0]],
+            ['2::-1', 1, '0:-1:-1', [0]],
+            ['2:-3:-1', 1, '0:-1:-1', [0]],
+            ['2::-1', 10, '2:-1:-1', [2, 1, 0]],
+            [':3:-1', 10, '9:3:-1', [9, 8, 7, 6, 5, 4]],
+        ];
+    }
+
+    public function dataProviderForIsSliceArrayTrue(): array
+    {
+        return [
+            [[]],
+            [[null, null]],
+            [[null, null, null]],
+            [[0]],
+            [[0, null]],
+            [[0, null, null]],
+            [[1, null, null]],
+            [[1, 0, null]],
+            [[1, 1, null]],
+            [[-1, 1, null]],
+            [[1, null, 1]],
+            [[1, null, 2]],
+            [[null, null, 1]],
+            [[null, null, -1]],
+            [[1, 10, -1]],
+        ];
+    }
+
+    public function dataProviderForIsSliceArrayFalse(): array
+    {
+        return [
+            [['']],
+            [['a']],
+            [[0, 1, 'a']],
+            [[0, 1, 2, 3]],
+            [[1.1, 1, 2]],
+            [[1, 1, 2.2]],
+            [null],
+            [0],
+            [1],
+            [0.0],
+            [1.0],
+            [true],
+            [false],
+            [new \ArrayObject([])],
+            [['a' => 1]],
+            [[[]]],
+            [[['a' => 1]]],
+        ];
+    }
+}
