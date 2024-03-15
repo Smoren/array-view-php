7ecb542d4d20771706e8fe2956a05de8ffec7504
Smoren <ofigate@gmail.com>
2024-03-15 19:26:24 +0300

    Arrays as selectors support added.

--- tests/unit/ArrayMaskView/IssetTest.php
+++ tests/unit/ArrayMaskView/IssetTest.php
@@ -17,57 +17,159 @@ class IssetTest extends \Codeception\Test\Unit
         $view = ArrayView::toView($source);
 
         $this->assertTrue(isset($view[new MaskSelector($boolMask)]));
+        $this->assertTrue(isset($view[$boolMask]));
+        $this->assertTrue(isset($view[ArrayView::toView($boolMask)]));
     }
 
     /**
      * @dataProvider dataProviderForIssetSelectorFalse
      */
-    public function testIssetSelectorFalse(array $source, array $indexes)
+    public function testIssetSelectorFalse(array $source, array $boolMask)
     {
         $view = ArrayView::toView($source);
 
-        $this->assertFalse(isset($view[new MaskSelector($indexes)]));
+        $this->assertFalse(isset($view[new MaskSelector($boolMask)]));
+        $this->assertFalse(isset($view[$boolMask]));
+        $this->assertFalse(isset($view[ArrayView::toView($boolMask)]));
 
         $this->expectException(SizeError::class);
-        $_ = $view[new MaskSelector($indexes)];
+        $_ = $view[new MaskSelector($boolMask)];
     }
 
     public function dataProviderForIssetSelectorTrue(): array
     {
         return [
-            [[], [], []],
-            [[1], [0], []],
-            [[1, 2, 3], [0, 0, 0], []],
-            [[1], [1], [1]],
-            [[1, 2], [1, 0], [1]],
-            [[1, 2], [0, 1], [2]],
-            [[1, 2], [1, 1], [1, 2]],
-            [[1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 0, 1, 0, 1, 0, 1, 0], [2, 4, 6, 8]],
-            [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 1, 1, 0, 0, 0, 0, 0, 1], [1, 2, 3, 9]],
+            [
+                [],
+                [],
+                [],
+            ],
+            [
+                [1],
+                [false],
+                [],
+            ],
+            [
+                [1, 2, 3],
+                [false, false, false],
+                [],
+            ],
+            [
+                [1],
+                [true],
+                [1],
+            ],
+            [
+                [1, 2],
+                [true, false],
+                [1],
+            ],
+            [
+                [1, 2],
+                [false, true],
+                [2],
+            ],
+            [
+                [1, 2],
+                [true, true],
+                [1, 2],
+            ],
+            [
+                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                [false, true, false, true, false, true, false, true, false],
+                [2, 4, 6, 8],
+            ],
+            [
+                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                [true, true, true, false, false, false, false, false, true],
+                [1, 2, 3, 9],
+            ],
         ];
     }
 
     public function dataProviderForIssetSelectorFalse(): array
     {
         return [
-            [[], [0], []],
-            [[], [1], []],
-            [[], [0, 1], []],
-            [[1], [], []],
-            [[1], [0, 0], []],
-            [[1], [1, 0], []],
-            [[1], [1, 1, 1], []],
-            [[1, 2, 3], [], []],
-            [[1, 2, 3], [0], []],
-            [[1, 2, 3], [0, 0], []],
-            [[1, 2, 3], [0, 0, 0, 0], []],
-            [[1, 2, 3], [0, 0, 0, 0, 0], []],
-            [[1, 2, 3], [1], []],
-            [[1, 2, 3], [1, 1], []],
-            [[1, 2, 3], [1, 1, 1, 1], []],
-            [[1, 2, 3], [1, 1, 1, 1, 1], []],
-            [[1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 0, 1, 0, 1, 0, 1], [2, 4, 6, 8]],
-            [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 1, 1, 0, 0, 0, 0, 0, 1, 0], [1, 2, 3, 9]],
+            [
+                [],
+                [false],
+                [],
+            ],
+            [
+                [],
+                [true],
+                [],
+            ],
+            [
+                [],
+                [false, true],
+                [],
+            ],
+            [
+                [1],
+                [false, false],
+                [],
+            ],
+            [
+                [1],
+                [true, false],
+                [],
+            ],
+            [
+                [1],
+                [true, true, true],
+                [],
+            ],
+            [
+                [1, 2, 3],
+                [false],
+                [],
+            ],
+            [
+                [1, 2, 3],
+                [false, false],
+                [],
+            ],
+            [
+                [1, 2, 3],
+                [false, false, false, false],
+                [],
+            ],
+            [
+                [1, 2, 3],
+                [false, false, false, false, false],
+                [],
+            ],
+            [
+                [1, 2, 3],
+                [true],
+                [],
+            ],
+            [
+                [1, 2, 3],
+                [true, true],
+                [],
+            ],
+            [
+                [1, 2, 3],
+                [true, true, true, true],
+                [],
+            ],
+            [
+                [1, 2, 3],
+                [true, true, true, true, true],
+                [],
+            ],
+            [
+                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                [false, true, false, true, false, true, false, true],
+                [2, 4, 6, 8],
+            ],
+            [
+                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                [true, true, true, false, false, false, false, false, true, false],
+                [1, 2, 3, 9],
+            ],
         ];
     }
 }
