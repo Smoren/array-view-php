1735d99917273610b298e31dbfabb0628f25f1d0
Smoren <ofigate@gmail.com>
2024-03-16 18:09:43 +0300

    More benchs.

--- tests/unit/Examples/BenchTest.php
+++ tests/unit/Examples/BenchTest.php
@@ -8,9 +8,41 @@ use Smoren\ArrayView\Views\ArrayView;
 
 class BenchTest extends \Codeception\Test\Unit
 {
+    private $n = 10000;
+
+    public function testReadSliceView()
+    {
+        $n = $this->n;
+        $originalArray = range(0, $n);
+
+        $ts = \microtime(true);
+        $view = ArrayView::toView($originalArray);
+        $result = $view['::2'];
+        $spent = \round(\microtime(true) - $ts, 4);
+
+        echo "[testReadSliceView] SPENT: {$spent} s\n";
+        ob_flush();
+    }
+
+    public function testReadSlicePure()
+    {
+        $n = $this->n;
+        $n_2 = intval($n / 2);
+        $originalArray = range(0, $n);
+
+        $ts = \microtime(true);
+        $result = [];
+        for ($i = 0; $i < $n_2; $i++) {
+            $result[$i] = $originalArray[$i * 2];
+        }
+        $spent = \round(\microtime(true) - $ts, 4);
+
+        echo "[testReadSlicePure] SPENT: {$spent} s\n";
+        ob_flush();
+    }
     public function testWriteSliceView()
     {
-        $n = 10000;
+        $n = $this->n;
         $n_2 = intval($n / 2);
         $originalArray = range(0, $n);
         $toWrite = range(0, $n_2);
@@ -20,13 +52,13 @@ class BenchTest extends \Codeception\Test\Unit
         $view['::2'] = $toWrite;
         $spent = \round(\microtime(true) - $ts, 4);
 
-        echo "SPENT: {$spent} s\n";
+        echo "[testWriteSliceView] SPENT: {$spent} s\n";
         ob_flush();
     }
 
     public function testWriteSlicePure()
     {
-        $n = 10000;
+        $n = $this->n;
         $n_2 = intval($n / 2);
         $originalArray = range(0, $n);
         $toWrite = range(0, $n_2);
@@ -37,7 +69,7 @@ class BenchTest extends \Codeception\Test\Unit
         }
         $spent = \round(\microtime(true) - $ts, 4);
 
-        echo "SPENT: {$spent} s\n";
+        echo "[testWriteSlicePure] SPENT: {$spent} s\n";
         ob_flush();
     }
 }
