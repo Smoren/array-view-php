e5d80b5c84fba2fd8888fe75ab59fcd9924fe1a2
Smoren <ofigate@gmail.com>
2024-03-17 19:30:54 +0300

    More docs for selectors.

--- src/Selectors/SliceSelector.php
+++ src/Selectors/SliceSelector.php
@@ -11,6 +11,32 @@ use Smoren\ArrayView\Views\ArraySliceView;
 
 /**
  * Represents a slice selector that selects elements based on the provided slice parameters.
+ *
+ * ##### Example
+ *  ```php
+ * $originalArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ * $view = ArrayView::toView($originalArray);
+ *
+ * $selector = new SliceSelector('::2');
+ * print_r($view[$selector]); // [1, 3, 5, 7, 9]
+ * print_r($view->subview($selector)->toArray()); // [1, 3, 5, 7, 9]
+ *
+ * $selector = new SliceSelector('1::2');
+ * print_r($view[$selector]); // [2, 4, 6, 8, 10]
+ * print_r($view->subview($selector)->toArray()); // [2, 4, 6, 8, 10]
+ *
+ * $selector = new SliceSelector('-3::-2');
+ * print_r($view[$selector]); // [8, 6, 4, 2]
+ * print_r($view->subview($selector)->toArray()); // [8, 6, 4, 2]
+ *
+ * $selector = new SliceSelector('1:4');
+ * print_r($view[$selector]); // [2, 3, 4]
+ * print_r($view->subview($selector)->toArray()); // [2, 3, 4]
+ *
+ * $selector = new SliceSelector('-2:0:-1');
+ * print_r($view[$selector]); // [9, 8, 7, 6, 5, 4, 3, 2]
+ * print_r($view->subview($selector)->toArray()); // [9, 8, 7, 6, 5, 4, 3, 2]
+ * ```
  */
 class SliceSelector extends Slice implements ArraySelectorInterface
 {
